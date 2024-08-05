9c6e675794d93801a3d0620723bd3ce8f7038f74
Summer Davis <summer.davis1819@gmail.com>
2024-08-05 19:03:14 +0000

    learning about input function

---
+++ function.py
@@ -0,0 +1,8 @@
+dat_input = input("What is your name? ")
+
+def greet(name):
+    # any code indented under the function
+    # will be code that belongs to that function
+    return "Hello, " + name
+
+print(greet(dat_input))
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Please enter your favorite movie: ")
+    
+    ## the line below creates a single string that is passed to print()
+    # print("You told me your favorite movie is: " + user_input)
+    
+    ## print() can be given a series of objects separated by a comma
+    print("You told me your favorite movie is:", user_input)
+
+    theater_input = input("What is your movie theater of choice? ")
+    print("You told me your movie theater of choice is:", theater_input)
+
+main()
+
