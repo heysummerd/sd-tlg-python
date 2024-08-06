105e4353197798d21a5d232c67d903b92b1001b4
Summer Davis <summer.davis1819@gmail.com>
2024-08-06 14:19:18 +0000

    Tuesday Morning Challenge

---
+++ tuesdaymorningchallenge.py
@@ -0,0 +1,21 @@
+#!/usr/env/python3
+
+def main():
+    students = [
+        "Amir", "Breana", "Katie", "Clayton", "Coty", "Daiyron",
+        "Douglas", "Gabriel", "Jakira", "John", "Jonathan",
+        "Justin", "Megan", "Tayo", "Summer", "Tomiwa"]
+
+    # len() function counts up how many names in the list
+    headcount = len(students)
+
+    str_number = input(f"Pick a number between 1 and {headcount}: ")
+
+    # int() function converts a string into an integer
+    int_number = int(str_number)
+
+    student_choice = students[int_number - 1]
+
+    print(f"{student_choice} is AWESOME!")
+
+main()
